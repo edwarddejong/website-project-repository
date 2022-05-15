8cacd366a28d32eee337503888cbcec4348da7d4
edwarddejong <edward.dejong@protonmail.com>
2022-05-15 13:26:06 +1000

    Added further imagery to webpage, and added form styling

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+
+form * {
+    display: block;
+    width: 80%;
+    margin: 1em;
+  }
+  
+  #checkbox {
+    display:inline;
+  
+  }
