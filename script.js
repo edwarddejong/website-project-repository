fe8571c28d4db145b7a2f36189c5cebae759aa03
edwarddejong <edward.dejong@protonmail.com>
2022-05-15 18:55:56 +1000

    Added logo, adjusted imagery positioning in style.css

--- script.js
+++ script.js
@@ -3,15 +3,3 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
-
-
-form * {
-    display: block;
-    width: 80%;
-    margin: 1em;
-  }
-  
-  #checkbox {
-    display:inline;
-  
-  }
