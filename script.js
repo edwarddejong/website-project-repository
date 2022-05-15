dc2437d7bf48ab76f3a7d6c2caa77f027c42a7cc
edwarddejong <edward.dejong@protonmail.com>
2022-05-15 13:29:36 +1000

    Further styling

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+
 
 form * {
     display: block;
